82d2ec4b238df97acecc46faeebe8a42c06d312a
FelipeFranke5 <frankefelipee@gmail.com>
2024-01-22 11:52:32 -0300

    removed STATICFILES_DIRS from settings.py

--- core/settings.py
+++ core/settings.py
@@ -146,11 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
-
-if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / "static/"]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
